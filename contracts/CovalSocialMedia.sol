ff9e1b0dd12abf8493fb288562fd8ef2587a5cff
Samuel1-ona <samuonanikesamuel123@gmail.com>
2024-02-27 15:53:09 +0100

    setting up the nftfactory contract and access roles

--- contracts/CovalSocialMedia.sol
+++ contracts/CovalSocialMedia.sol
@@ -7,6 +7,11 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract CovalSocialMedia is AccessControl{
 
+    // let define the role of the RBAC
+
+    bytes32 public constant AMIN_ROLE = keccak256("AMIN_ROLE");
+    bytes32 PUBLIC constant USER_ROLE = keccak256("USER_ROLE");
+
     CovalNftFactory public covalnftFactory;
 
     // Let create a state variable that keep tract of posts
@@ -25,6 +30,16 @@ contract CovalSocialMedia is AccessControl{
 // mapping that tract the post Id
 
 mapping(uint256 => post)private posts;
+
+event postCreated(uint8 nftid, string postid, string author);
+
+// setting the roles and calling the nft
+constructor (address _covalNftaddress){
+    covalnftFactory = CovalNftFactory(_covalNftaddress);
+    _setupRole(AMIN_ROLE,msg.sender);
+    _setAdminRole(USER_ROLE, AMIN_ROLE);
+}
+
 
 
     
