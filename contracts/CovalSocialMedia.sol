c8b69910bdeaef5efb996904f7b6b48088dab209
Samuel1-ona <samuonanikesamuel123@gmail.com>
2024-02-27 16:02:44 +0100

    created a function to create a post and add new users

--- contracts/CovalSocialMedia.sol
+++ contracts/CovalSocialMedia.sol
@@ -16,7 +16,7 @@ contract CovalSocialMedia is AccessControl{
 
     // Let create a state variable that keep tract of posts
 
-    uint256 private postcounter = 0;
+    uint256 private postidcounter = 0;
 
 
 // struct representing the post
@@ -40,7 +40,26 @@ constructor (address _covalNftaddress){
     _setAdminRole(USER_ROLE, AMIN_ROLE);
 }
 
+// creating a function to create a post from the factory contract
+// because every post is a nft
+function createPost(string memory matadataURL ,  string memory content) external onlyRole(USER_ROLE) {
 
+    uint8 nftid = covalnftFactory.mintNFT(msg.sender, matadataURL);
 
+    // incrementing the post when the post is created 
+    postidcounter++;
+
+    post[postidcounter] = post(nftid, content, msg.sender, block.timestamp);
+
+    emit postCreated(nftid, postidcounter, msg.sender);
+}
+
+// function to assign user role to new users
+
+function addNewuser(address _newUser) external onlyRole(AMIN_ROLE) {
+
+    grantRole(USER_ROLE, _newUser);
+    
+}
     
 }
